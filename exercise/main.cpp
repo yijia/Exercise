41c847a7008299707df9e46edb10c9b8235a98bf
yijia <yijia@adobe.com>
2014-06-22 20:37:08 +0800

    Initial Commit

---
+++ exercise/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  exercise
+//
+//  Created by labuser on 6/22/14.
+//  Copyright (c) 2014 labuser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
